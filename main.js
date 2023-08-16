7b5298cc125a8e0166478182edbea4ba00b01d39
Fukuda-B <60131202+Fukuda-B@users.noreply.github.com>
2023-08-17 02:15:56 +0900

    up

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 // https://developer.mozilla.org/ja/docs/Web/API/Web_Audio_API
+// https://qiita.com/tomoya_ozawa/items/bfd09b436075af916833
+// https://sleepygamersmemo.blogspot.com/2020/02/virtual-piano-memo.html
 
 let keyArr = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
+let volume = document.getElementById('volume');
+let tone_length = document.getElementById('tone_length');
+
 const sound = function (key, oct) {
     const audioContext = new (window.AudioContext || window.webkitAudioContext)();
     const oscillator = audioContext.createOscillator();
@@ -8,23 +13,78 @@ const sound = function (key, oct) {
     let freqType = document.getElementById('freq_type').value;
     oscillator.type = freqType;
 
-    let frequency = parseInt(440*Math.pow(2,((keyArr.indexOf(key)-9)/12)+(oct-4)));
+    let frequency = 440*Math.pow(2,((keyArr.indexOf(key)-9)/12)+(oct-4));
     oscillator.frequency.value = frequency;
+    console.log(freqType+' '+frequency+'Hz');
 
-    // let gainNode = audioContext.createGain();
-    // gainNode.gain.setValueAtTime(0, play.count);
-    // gainNode.gain.linearRampToValueAtTime(1.0, play.count + 0.01);
-    // gainNode.gain.linearRampToValueAtTime(0.7, play.count + 0.20);
-    // gainNode.gain.linearRampToValueAtTime(0.4, play.count + 0.40);
-    // gainNode.gain.linearRampToValueAtTime(0.0, play.count + 0.80);
+    let gainNode = audioContext.createGain();
+    let gainValue = document.getElementById('volume').value;
+    gainNode.gain.setValueAtTime(gainValue/100, audioContext.currentTime);
 
-    // oscillator.connect(gainNode);
-    // gainNode.connect(audioContext.destination);
+    oscillator.connect(gainNode);
+    gainNode.connect(audioContext.destination);
+    oscillator.start = oscillator.start || oscillator.noteOn;
     oscillator.start();
     // oscillator.start(play.count || 0);
     // return play;
+
+    setTimeout(() => {
+        // プツプツ音がするので，ちょっと伸ばす
+        gainNode.gain.setValueAtTime(gainNode.gain.value, audioContext.currentTime);
+        gainNode.gain.linearRampToValueAtTime(0.0, audioContext.currentTime + 0.4);
+    }, tone_length.value);
+    setTimeout(() => {
+        oscillator.stop();
+    }, tone_length.value+400);
 }
 
-document.getElementById('start').addEventListener('click', () => {
-    sound('A', 4);
+// 読み込み時
+window.addEventListener('load', () => {
+    document.getElementById('current_volume').innerText = volume.value;
+    document.getElementById('current_tone_length').innerText = tone_length.value;
+});
+
+volume.addEventListener('input', () => {
+    document.getElementById('current_volume').innerText = volume.value;
+});
+
+tone_length.addEventListener('input', () => {
+    document.getElementById('current_tone_length').innerText = tone_length.value;
+});
+
+/*
+    キー配置は下と同じにしてある
+    https://mike3.net/piano/1-piano.html
+
+    こっちに合わせようか悩んだが
+    https://onlinepiano.app/ja/#
+*/
+let keyMap = {
+    'Digit1': ['Ab', 3],
+    'KeyQ'  : ['A' , 3],
+    'Digit2': ['Bb', 3],
+    'KeyW'  : ['B' , 3],
+    'KeyE'  : ['C' , 4],
+    'Digit4': ['Db', 4],
+    'KeyR'  : ['D' , 4],
+    'Digit5': ['Eb', 4],
+    'KeyT'  : ['E' , 4],
+    'KeyY'  : ['F' , 4],
+    'Digit7': ['Gb', 4],
+    'KeyU'  : ['G' , 4],
+    'Digit8': ['Ab', 4],
+    'KeyI'  : ['A' , 4],
+    'Digit9': ['Bb', 4],
+    'KeyO'  : ['B' , 4],
+    'KeyP'  : ['C' , 5],
+    'Minus' : ['Db' , 5],
+    'BracketLeft': ['D', 5],
+    'Equal' : ['Eb', 5],
+    'BracketRight': ['E', 5],
+}
+window.addEventListener('keydown', (e) => {
+    if (e.repeat) {return;}
+    if (e.code in keyMap) {
+        sound(keyMap[e.code][0], keyMap[e.code][1])
+    }
 })
